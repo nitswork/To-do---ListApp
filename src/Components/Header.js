df4642623b2d8fa6ab7462e2ce6b8fdd500d3550
nitswork <nr25122022@gmail.com>
2025-06-03 15:47:17 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+// import "./Header.css";
+const Header = () =>{
+    return <nav> Get Ready to Complete Your Daily Goals </nav>;
+};
+export default Header;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,52 @@
+import React , {useEffect,useState}from "react";
+import Task from "./Task";
+
+const Home =() => {
+    const initialArr = localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[];
+    // const [tasks,setTasks] = useState([]);
+    const [tasks,setTasks] = useState(initialArr);
+    const [title,setTitle] = useState("");
+    const [description,setDescription] = useState("");
+    console.log(...tasks);
+    console.log(title,description);
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        setTasks([...tasks,{title,description}]);
+        setTitle("");
+        setDescription("");
+    };
+    const deleteTask =(index) =>{
+        const filteredArr = tasks.filter((val,i)=>{
+            return i!== index;
+        });
+        //console.log(filteredArr);
+        setTasks(filteredArr);
+    };
+
+    useEffect(()=>{
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+        }, [tasks]);
+    return <div className="container">
+        <h1> DAILY GOALS </h1>
+        <form onSubmit={submitHandler}>
+            <input type="text" 
+            placeholder="Title" 
+            value = {title} 
+            onChange ={(e) =>setTitle(e.target.value)}
+            />
+            <textarea placeholder="Description"
+            value = {description} 
+            onChange ={(e) =>setDescription(e.target.value)}
+            ></textarea>
+            <button type="submit">ADD</button>
+        </form>
+
+        {tasks.map((item,index)=>(
+            <Task key={index} title={item.title} description = {item.description}
+            deleteTask={deleteTask}
+            index={index}/>
+        ))}
+    </div>;
+};
+export default Home;
